d74e400f6ad19e04ddb4a865dec48135c756c96c
David Davidson <ddavidson644@gmail.com>
2026-03-02 22:15:32 -0500

    Implementing the LoanProcessor thread class

--- src/main/java/com/examplecode/LoanProcessor.java
+++ src/main/java/com/examplecode/LoanProcessor.java
@@ -1,8 +1,21 @@
 package com.examplecode;
 
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class LoanProcessor implements Runnable{
+
+    private final Loan loan;
+
+    public LoanProcessor(Loan loan) {
+        this.loan = loan;
+    }
+
+    static AtomicReference<BigDecimal> totalCollected;
+
     @Override
     public void run(){
-
+        for(int i = 1; i <= loan.getTerm(); i++){
+        }
     }
 }
